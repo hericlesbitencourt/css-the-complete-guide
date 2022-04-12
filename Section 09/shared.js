f7573e79f5b86171e1b3d5a4b6903ae2d661bff8
Hericles Bitencourt <hericlesbr@gmail.com>
2022-04-12 13:20:04 -0300

    Section 9 - Lesson 135

--- Section 09/shared.js
+++ Section 09/shared.js
@@ -1,7 +1,18 @@
 var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+console.dir(selectPlanButtons)
+
+for (var i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener('click', function() {
+        modal.style.display = 'block'
+        backdrop.style.display = 'block'
+    })
+}
+ 
 // var backdrop = document.querySelectorAll('.backdrop');
 
 // console.log(backdrop)
 // console.dir(backdrop)
 
-backdrop.style.display = 'block'
+// backdrop.style.display = 'block'
