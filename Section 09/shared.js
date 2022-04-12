1d56f3e493649a7ad2b99b0b87372127dc2e243b
Hericles Bitencourt <hericlesbr@gmail.com>
2022-04-12 13:23:53 -0300

    Section 9 - Assignment 4

--- Section 09/shared.js
+++ Section 09/shared.js
@@ -1,5 +1,6 @@
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
+var modalNoButton = document.querySelector('.modal__action--negative');
 var selectPlanButtons = document.querySelectorAll('.plan button');
 console.dir(selectPlanButtons)
 
@@ -9,7 +10,15 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
         backdrop.style.display = 'block'
     })
 }
- 
+
+backdrop.addEventListener('click', closeModal)
+
+modalNoButton.addEventListener('click', closeModal)
+
+function closeModal() {
+    backdrop.style.display = 'none'
+    modal.style.display = 'none'
+}
 // var backdrop = document.querySelectorAll('.backdrop');
 
 // console.log(backdrop)
