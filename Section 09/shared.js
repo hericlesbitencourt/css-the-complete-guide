17b782c86b82c9d77d9db4e6ac1be03c8c3dae67
Hericles Bitencourt <hericlesbr@gmail.com>
2022-04-12 13:14:40 -0300

    Section 9 - Lesson 134

---
+++ Section 09/shared.js
@@ -0,0 +1,7 @@
+var backdrop = document.querySelector('.backdrop');
+// var backdrop = document.querySelectorAll('.backdrop');
+
+// console.log(backdrop)
+// console.dir(backdrop)
+
+backdrop.style.display = 'block'
